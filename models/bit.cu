709ff1fd510d4d63390a411a3d94354686c23928
Kawamura_Kazuma <103241727+uj151133@users.noreply.github.com>
2024-06-02 07:53:06 +0900

    add cach

---
+++ models/bit.cu
@@ -0,0 +1,22 @@
+#inslude "bit.h"
+
+#include <cuda_runtime.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+__constant__ int  KET_0[1][2] = {
+    {1},
+    {0}
+};
+
+__constant__ int  KET_1[1][2] = {
+    {0},
+    {1}
+};
+
+__constant__ int  BRA_0[2] = {1, 0};
+
+__constant__ int  BRA_1[2] = {0, 1};
