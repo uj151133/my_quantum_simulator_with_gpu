30f9aae7bc00b09078f2996ccf6615c27ffbeb2f
Kawamura_Kazuma <103241727+uj151133@users.noreply.github.com>
2024-06-06 05:07:51 +0900

    update

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 # from models import node
 import math
+
+
+# import calculation
 import gate
+# from sympy import symbols, sin, cos
+# from sympy2ginac import sympy_to_ginac
 
-print(gate.I_GATE)
-print(gate.NOT_GATE)
-print(gate.Z_GATE)
+# # シンボリック変数の定義
+# x, y = symbols('x y')
+
+# # 数式の定義
+# expr1 = sin(x) * cos(y)
+# expr2 = sin(x) * sin(y)
+
+# # sympyの式をGiNaCの式に変換
+# ginac_expr1 = sympy_to_ginac(expr1)
+# ginac_expr2 = sympy_to_ginac(expr2)
+
+# # 共通因数を計算
+# common_factor_result = common_factor.common_factor(ginac_expr1, ginac_expr2)
+# print("共通因数:", common_factor_result)
 print(gate.HADAMARD_GATE)
-print(gate.CNOT_GATE)
-print(gate.CZ_GATE)
-
-print(gate.Ry(math.pi))
