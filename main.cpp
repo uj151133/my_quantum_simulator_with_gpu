d837ceb0be22ea178c7ed1d7a5b9629da7b815da
Kawamura_Kazuma <103241727+uj151133@users.noreply.github.com>
2024-06-14 10:37:00 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include "iostream"
-#include "myadd.hpp"
-#include "gate.hpp"
+#include <ginac/ginac.h>
+#include "src/models/bit.hpp"
+#include "src/models/gate.hpp"
+
+using namespace GiNaC;
 
 int main() {
-    int result = add(2, 3);
-    std::cout << "Result of add(2, 3) is: " << result << std::endl;
+    matrix h = X_GATE;
+    std::cout << "Result is: " << Y_GATE << std::endl;
+    for (unsigned i = 0; i < X_GATE.rows(); ++i) {
+        for (unsigned j = 0; j < X_GATE.cols(); ++j) {
+            std::cout << X_GATE(i, j) << " ";
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
