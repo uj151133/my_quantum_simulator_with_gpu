d2d0d718cbe3594075230b6e9f98e0e2290b0757
Kawamura_Kazuma <103241727+uj151133@users.noreply.github.com>
2024-07-28 17:40:49 +0900

    update

--- src/models/bit.cpp
+++ src/models/bit.cpp
@@ -9,58 +9,70 @@
 using namespace std;
 using namespace GiNaC;
 
-QMDDEdge setKet0() {
-    QMDDNode* ket0 = new QMDDNode(2);
-    ket0->edges[0] = QMDDEdge(1, nullptr);
-    ket0->edges[1] = QMDDEdge(0, nullptr);
-    QMDDEdge ket0Edge(1, ket0);
-    return ket0Edge;
-}
+const QMDDState state::KET_0 = [] {
+    auto ket0Node = make_shared<QMDDNode>(2);
 
-const matrix KET_0 = matrix{
-{1},
-{0}
-};
+    ket0Node->edges[0] = QMDDEdge(1, nullptr);
+    ket0Node->edges[1] = QMDDEdge(0, nullptr);
 
-const matrix KET_1 = matrix{
-{0},
-{1}
-};
+    QMDDEdge ket0Edge(1, ket0Node);
+    return QMDDState(ket0Edge);
+}();
 
-const matrix KET_PLUS = matrix{
-{sqrt(ex(2))},
-{sqrt(ex(2))}
-};
+// QMDDState setKet0() {
+//     auto ket0Node = make_shared<QMDDNode>(2);
 
-const matrix KET_MINUS = matrix{
-{sqrt(ex(2))},
-{-sqrt(ex(2))}
-};
+//     ket0Node->edges[0] = QMDDEdge(1, nullptr);
+//     ket0Node->edges[1] = QMDDEdge(0, nullptr);
 
-const matrix BRA_0 = matrix{
-{1, 0}
-};
+//     QMDDEdge ket0Edge(1, ket0Node);
+//     return QMDDState(ket0Edge);
+// }
 
-const matrix BRA_1 = matrix{
-{0, 1}
-};
+// const matrix KET_0 = matrix{
+// {1},
+// {0}
+// };
 
-const matrix BRA_PLUS = matrix{
-{sqrt(ex(2)), sqrt(ex(2))}
-};
+// const matrix KET_1 = matrix{
+// {0},
+// {1}
+// };
 
-const matrix BRA_MINUS = matrix{
-{sqrt(ex(2)), -sqrt(ex(2))}
-};
+// const matrix KET_PLUS = matrix{
+// {sqrt(ex(2))},
+// {sqrt(ex(2))}
+// };
 
-PYBIND11_MODULE(bit, m) {
+// const matrix KET_MINUS = matrix{
+// {sqrt(ex(2))},
+// {-sqrt(ex(2))}
+// };
 
-    m.attr("KET_0") = KET_0;
-    m.attr("KET_1") = KET_1;
-    m.attr("KET_PLUS") = KET_PLUS;
-    m.attr("KET_MINUS") = KET_MINUS;
-    m.attr("BRA_0") = BRA_0;
-    m.attr("BRA_1") = BRA_1;
-    m.attr("BRA_PLUS") = BRA_PLUS;
-    m.attr("BRA_MINUS") = BRA_MINUS;
-}
+// const matrix BRA_0 = matrix{
+// {1, 0}
+// };
+
+// const matrix BRA_1 = matrix{
+// {0, 1}
+// };
+
+// const matrix BRA_PLUS = matrix{
+// {sqrt(ex(2)), sqrt(ex(2))}
+// };
+
+// const matrix BRA_MINUS = matrix{
+// {sqrt(ex(2)), -sqrt(ex(2))}
+// };
+
+// PYBIND11_MODULE(bit, m) {
+
+//     m.attr("KET_0") = KET_0;
+//     m.attr("KET_1") = KET_1;
+//     m.attr("KET_PLUS") = KET_PLUS;
+//     m.attr("KET_MINUS") = KET_MINUS;
+//     m.attr("BRA_0") = BRA_0;
+//     m.attr("BRA_1") = BRA_1;
+//     m.attr("BRA_PLUS") = BRA_PLUS;
+//     m.attr("BRA_MINUS") = BRA_MINUS;
+// }
--- src/models/qmdd.hpp
+++ src/models/qmdd.hpp
@@ -17,11 +17,11 @@ struct QMDDNode; // 前方宣言
 struct QMDDEdge {
     complex<double> weight; // エッジの重み
     bool isTerminal; // 終端ノードかどうか
-    // unique_ptr<QMDDNode> node; // エッジの指すQMDDNodeのポインタ
-    QMDDNode* node;
+    shared_ptr<QMDDNode> node; // エッジの指すQMDDNodeのポインタ
+    // QMDDNode* node;
     
-    QMDDEdge(complex<double> w = {0, 0}, QMDDNode* n = nullptr);
-    ~QMDDEdge();
+    QMDDEdge(complex<double> w = {0, 0}, shared_ptr<QMDDNode> n = nullptr);
+    ~QMDDEdge() = default;
     bool operator==(const QMDDEdge& other) const;
     friend ostream& operator<<(ostream& os, const QMDDEdge& edge);
 };
@@ -31,11 +31,13 @@ struct QMDDNode {
     size_t uniqueTableKey; // ユニークテーブルのキー
 
     QMDDNode(size_t numEdges = 2);
-    ~QMDDNode();
-    QMDDNode(const QMDDNode&);
-    QMDDNode& operator=(const QMDDNode&);
-    QMDDNode(QMDDNode&& other) noexcept; // ムーブコンストラクタ
-    QMDDNode& operator=(QMDDNode&& other) noexcept; // ムーブ代入演算子
+    ~QMDDNode() = default;
+    // コピーコンストラクタとコピー代入演算子
+    QMDDNode(const QMDDNode& other) = default;
+    QMDDNode& operator=(const QMDDNode& other) = default;
+    // ムーブコンストラクタとムーブ代入演算子
+    QMDDNode(QMDDNode&& other) noexcept = default;
+    QMDDNode& operator=(QMDDNode&& other) noexcept = default; 
     bool operator==(const QMDDNode& other) const;
     friend ostream& operator<<(ostream& os, const QMDDNode& node);
 };
@@ -46,7 +48,7 @@ private:
 
 public:
     QMDDGate(QMDDEdge edge, size_t numEdges = 4);
-    ~QMDDGate();
+    ~QMDDGate() = default;
     QMDDNode* getStartNode() const;
     QMDDEdge getInitialEdge() const;
     friend ostream& operator<<(ostream& os, const QMDDGate& gate);
@@ -58,12 +60,12 @@ private:
 
 public:
     QMDDState(QMDDEdge edge, size_t numEdges = 2);
-    ~QMDDState();
+    ~QMDDState() = default;
     QMDDNode* getStartNode() const;
     QMDDEdge getInitialEdge() const;
     // QMDDState同士の足し算
     QMDDState operator+(const QMDDState& other);
-    QMDDNode* addNodes(QMDDNode* node1, QMDDNode* node2);
+    shared_ptr<QMDDNode> addNodes(QMDDNode* node1, QMDDNode* node2);
     friend ostream& operator<<(ostream& os, const QMDDState& state);
 };
 #endif // QMDD_HPP
