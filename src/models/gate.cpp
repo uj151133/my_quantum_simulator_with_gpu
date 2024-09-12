a86422db9f9c39a655163f5b189211a1d8a2fdcd
三石海人 <mitsuishikaito@mitsuishikaininnonotobukkukonpyuta.local>
2024-09-13 02:55:11 +0900

    check

--- src/models/gate.cpp
+++ src/models/gate.cpp
@@ -29,7 +29,7 @@ QMDDGate gate::Ph(double delta) {
 }
 
 QMDDGate gate::X() {
-    return QMDDGate(QMDDEdge(.0, make_shared<QMDDNode>(vector<vector<QMDDEdge>>{
+    return QMDDGate(QMDDEdge(1.0, make_shared<QMDDNode>(vector<vector<QMDDEdge>>{
         {QMDDEdge(.0, nullptr), QMDDEdge(1.0, nullptr)},
         {QMDDEdge(1.0, nullptr), QMDDEdge(.0, nullptr)}
     })));
