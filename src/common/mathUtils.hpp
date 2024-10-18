bbf562a476068a6f11e6c71b461f459148ea7c30
uj151133 <103241727+uj151133@users.noreply.github.com>
2024-10-18 23:42:02 +0900

    remove old one

--- src/common/mathUtils.hpp
+++ src/common/mathUtils.hpp
@@ -10,14 +10,9 @@
 using namespace std;
 
 namespace mathUtils {
-    QMDDEdge multiplication(const QMDDEdge& m, const QMDDEdge& v);
     QMDDEdge mul(const QMDDEdge& e0, const QMDDEdge& e1);
-    QMDDEdge addition(const QMDDEdge& e1, const QMDDEdge& e2);
     QMDDEdge add(const QMDDEdge& e0, const QMDDEdge& e1);
-    QMDDEdge kroneckerProduct(const QMDDEdge& e1, const QMDDEdge& e2);
     QMDDEdge kron(const QMDDEdge& e0, const QMDDEdge& e1);
-
-    QMDDEdge mulAny(QMDDEdge& e, int times);
 
     complex<double> csc(complex<double> theta);
     complex<double> sec(complex<double> theta);
