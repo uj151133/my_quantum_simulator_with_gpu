c7b0cc4519cb076cefbefd399c0d2e0927f37f14
uj151133 <uj151133@gmail.com>
2024-10-28 01:04:55 +0900

    increase gate swift

--- src/macOS/gate.swift
+++ src/macOS/gate.swift
@@ -27,4 +27,77 @@ enum gate {
             [edgeOne, edgeZero]
         ])))
     }
+
+    static func Y() -> QMDDGate {
+        return QMDDGate(edge: QMDDEdge(weight: -i, node: QMDDNode(edges: [
+            [edgeZero, edgeOne],
+            [QMDDEdge(weight: -1.0, node: nil), edgeZero]
+        ])))
+    }
+
+    static func Z() -> QMDDGate {
+        return QMDDGate(edge: QMDDEdge(weight: 1.0, node: QMDDNode(edges: [
+            [edgeOne, edgeZero],
+            [edgeZero, QMDDEdge(weight: -1.0, node: nil)]
+        ])))
+    }
+
+    static func S() -> QMDDGate {
+        return QMDDGate(edge: QMDDEdge(weight: 1.0, node: QMDDNode(edges: [
+            [edgeOne, edgeZero],
+            [edgeZero, QMDDEdge(weight: i, node: nil)]
+        ])))
+    }
+
+    static func Sdagger() -> QMDDGate {
+        return QMDDGate(edge: QMDDEdge(weight: 1.0, node: QMDDNode(edges: [
+            [edgeOne, edgeZero],
+            [edgeZero, QMDDEdge(weight: -i, node: nil)]
+        ])))
+    }
+
+    static func V() -> QMDDGate {
+        return QMDDGate(edge: QMDDEdge(weight: 1.0 / 2.0 + i / 2.0, node: QMDDNode(edges: [
+            [edgeOne, QMDDEdge(weight: i, node: nil)],
+            [QMDDEdge(weight: i, node: nil), QMDDEdge(weight: 0.5, node: nil)]
+        ])))
+    }
+
+    static func H() -> QMDDGate {
+        return QMDDGate(edge: QMDDEdge(weight: 1.0 / sqrt(2.0), node: QMDDNode(edges: [
+            [edgeOne, edgeOne],
+            [edgeOne, QMDDEdge(weight: -1.0, node: nil)]
+        ])))
+    }
+
+    static func CX1() -> QMDDGate {
+        return QMDDGate(edge: QMDDEdge(weight: 1.0, node: QMDDNode(edges: [
+            [gate.I().getInitialEdge(), edgeZero],
+            [edgeZero, gate.X().getInitialEdge()]
+        ])))
+    }
+
+    static func CX2() -> QMDDGate {
+        let cx2Node1 = QMDDNode(edges: [
+            [edgeOne, edgeZero],
+            [edgeZero, edgeZero]
+        ])
+
+        let cx2Node2 = QMDDNode(edges: [
+            [edgeZero, edgeZero],
+            [edgeZero, edgeOne]
+        ])
+
+        return QMDDGate(edge: QMDDEdge(weight: 1.0, node: QMDDNode(edges: [
+            [QMDDEdge(weight: 1.0, node: cx2Node1), QMDDEdge(weight: 1.0, node: cx2Node2)],
+            [QMDDEdge(weight: 1.0, node: cx2Node2), QMDDEdge(weight: 1.0, node: cx2Node1)]
+        ])))
+    }
+
+    static func varCX() -> QMDDGate {
+        return QMDDGate(edge: QMDDEdge(weight: 1.0, node: QMDDNode(edges: [
+            [gate.X().getInitialEdge(), edgeZero],
+            [edgeZero, gate.I().getInitialEdge()]
+        ])))
+    }
 }
