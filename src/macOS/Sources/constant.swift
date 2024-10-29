7b2a0820401b352256596a9d448c09ff81c24713
uj151133 <uj151133@gmail.com>
2024-10-29 16:57:45 +0900

    update

---
+++ src/macOS/Sources/constant.swift
@@ -0,0 +1,7 @@
+import Foundation
+import Numerics
+
+let i = Complex<Double>(0.0, 1.0)
+
+let edgeZero = QMDDEdge(weight: Complex<Double>(0.0, 0.0), node: nil)
+let edgeOne = QMDDEdge(weight: Complex<Double>(1.0, 0.0), node: nil)
--- src/macOS/Sources/gate.swift
+++ src/macOS/Sources/gate.swift
@@ -1,11 +1,6 @@
 import Foundation
 import Numerics
 
-let i = Complex<Double>(0.0, 1.0)
-let edgeOne = QMDDEdge(weight: 1.0, node: nil)
-let edgeZero = QMDDEdge(weight: 0.0, node: nil)
-
-// gate名前空間の定義
 enum gate {
     static func I() -> QMDDGate {
         return QMDDGate(edge: QMDDEdge(weight: 1.0, node: QMDDNode(edges: [
