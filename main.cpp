c1f4f558a3506672590df535805f371707cbfbf4
uj151133 <uj151133@gmail.com>
2024-11-23 18:03:14 +0900

    update

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ void execute() {
 
     // UniqueTable& uniqueTable = UniqueTable::getInstance();
 
-    int numQubits = 13;
+    int numQubits = 14;
     int omega = std::pow(2, numQubits) - 1;
 
     grover(numQubits, omega);
