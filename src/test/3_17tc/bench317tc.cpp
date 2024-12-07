287a0fb0128186eb63af83bf5d0a0c35774497dc
uj151133 <uj151133@gmail.com>
2024-12-07 19:17:21 +0900

    update merging thread

--- src/test/3_17tc/bench317tc.cpp
+++ src/test/3_17tc/bench317tc.cpp
@@ -2,8 +2,8 @@
 
 void bench317tc() {
     QMDDState ket_0 = state::Ket0();
-    QMDDEdge firstEdge = mathUtils::kronParallel(ket_0.getInitialEdge(), ket_0.getInitialEdge());
-    firstEdge = mathUtils::kronParallel(firstEdge, ket_0.getInitialEdge());
+    QMDDEdge firstEdge = mathUtils::kron(ket_0.getInitialEdge(), ket_0.getInitialEdge());
+    firstEdge = mathUtils::kron(firstEdge, ket_0.getInitialEdge());
     QuantumCircuit circuit(3, QMDDState(firstEdge));
     circuit.addX(2);
     circuit.addCX(0, 2);
