189fc634fd631862ecee876f8944f1f4edeeed4e
uj151133 <uj151133@gmail.com>
2024-12-30 23:55:06 +0900

    update

---
+++ GUI/GUI/GUI/MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using GUI.Shared.Services;
+using GUI.Services;
+
+
+namespace GUI;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        // Add device-specific services used by the GUI.Shared project
+        builder.Services.AddSingleton<IFormFactor, FormFactor>();
+
+        builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
--- GUI/GUI/obj/Debug/net9.0-ios/iossimulator-arm64/nativelibraries/aot-output/arm64/aot-instances.dll.s
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:07a9a28832b417e72c789b0ba3a0b1792dd9b50a05a0413d3b7c4e93786d8fb1
-size 151735864
