a0f75cccfb7c627f117c5073586c08f54b68263b
uj151133 <uj151133@gmail.com>
2024-12-30 23:55:06 +0900

    update

---
+++ GUI/GUI/GUI.Shared/Services/IFormFactor.cs
@@ -0,0 +1,7 @@
+namespace GUI.Shared.Services;
+
+public interface IFormFactor
+{
+    public string GetFormFactor();
+    public string GetPlatform();
+}
--- GUI/GUI/obj/Debug/net9.0-maccatalyst/maccatalyst-arm64/nativelibraries/aot-output/arm64/aot-instances.dll.s
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:613e96c35237388455a6a28ec9a7528623f5ee4c1c9b37ec2766e2581abd72a0
-size 154763939
