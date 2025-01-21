a38f18c0081ee4149e4e16995c491417eafb6c1c
uj151133 <uj151133@gmail.com>
2025-01-21 21:09:55 +0900

    update ui

---
+++ GUI/GUI/GUI/obj/Debug/net9.0-android/assets/wwwroot/_content/GUI.Shared/Pages/Composer.razor.js
@@ -0,0 +1,11 @@
+// アイテムのリストを取得
+const gates = [...document.querySelectorAll(".h-gate")];
+
+const handleDragStart = (e) => e.target.classList.add("dragging");
+
+const handleDragEnd = (e) => e.target.classList.remove("dragging");
+
+for (const gate of gates) {
+  gate.addEventListener("dragstart", handleDragStart, false);
+  gate.addEventListener("dragend", handleDragEnd, false);
+}
