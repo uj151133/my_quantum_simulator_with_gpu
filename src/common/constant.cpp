7736fe2325bb77bd353a35991973224a818f1d6a
uj151133 <uj151133@gmail.com>
2025-05-20 23:10:25 +0900

    ok

--- src/common/constant.cpp
+++ src/common/constant.cpp
@@ -16,15 +16,13 @@ once_flag initEdgeFlag;
 once_flag initExtendedEdgeFlag;
 
 void initEdge() {
-
     i = complex<double>(.0, 1.0);
     edgeZero = QMDDEdge(.0, nullptr);
     edgeOne = QMDDEdge(1.0, nullptr);
 }
 
 void initExtendedEdge() {
-
     identityEdge = gate::I().getInitialEdge();
-    braketZero = mathUtils::mul(state::Ket0().getInitialEdge(), state::Bra0().getInitialEdge(), 3);
-    braketOne = mathUtils::mul(state::Ket1().getInitialEdge(), state::Bra1().getInitialEdge(), 3);
+    braketZero = mathUtils::dyad(state::Ket0().getInitialEdge(), state::Bra0().getInitialEdge());
+    braketOne = mathUtils::dyad(state::Ket1().getInitialEdge(), state::Bra1().getInitialEdge());
 }
