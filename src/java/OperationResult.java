d7a6f5f99c69682228c2c31ba3f7651860edb86f
uj151133 <uj151133@gmail.com>
2025-06-03 22:36:39 +0900

    update renew cache

--- src/java/OperationResult.java
+++ src/java/OperationResult.java
@@ -1,11 +1,1 @@
-public class OperationResult {
-    public final double real;
-    public final double imag;
-    public final long uniqueTableKey;
-
-    public OperationResult(double real, double imag, long uniqueTableKey) {
-        this.real = real;
-        this.imag = imag;
-        this.uniqueTableKey = uniqueTableKey;
-    }
-}
+public record OperationResult(double real, double imag, long uniqueTableKey) {}
