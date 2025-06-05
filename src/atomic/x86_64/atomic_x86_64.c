9c4cb8ae2f0bb5d470d5dc2fa3bc2d2b1e128164
uj151133 <uj151133@gmail.com>
2025-06-05 20:32:09 +0900

    update x86_64 of cas logic

--- src/atomic/x86_64/atomic_x86_64.c
+++ src/atomic/x86_64/atomic_x86_64.c
@@ -3,10 +3,5 @@
 extern bool cas_x86_64(void **ptr, void *expected, void *desired);
 
 bool cas(void **ptr, void *expected, void *desired) {
-    if (expected == NULL || ptr == NULL) {
-        fprintf(stderr, "Error: NULL pointer passed to cas_x86_64\n");
-        fprintf(stderr, "ptr: %p, expected: %p, desired: %p\n", ptr, expected, desired);
-        return false;
-    }
     return cas_x86_64(ptr, expected, desired);
 }
