39c6ae0b1b59fc5fb3aaf6209380eaa64e62429c
uj151133 <uj151133@gmail.com>
2025-10-23 15:49:15 +0000

    maybe past bench

--- main.cpp
+++ main.cpp
@@ -41,7 +41,7 @@ void execute() {
 
     // OperationCache& cache = OperationCache::getInstance();
 
-    int numQubits = 7;
+    int numQubits = 13;
     int numGates = 200;
 
     randomRotate(numQubits, numGates);
